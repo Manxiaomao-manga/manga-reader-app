0981980e1ac60656e6e59e3afa7a08bcffafc6ff
dean200416 <liu200416@gmail.com>
2026-07-25 16:43:24 +0800

    Fix: status bar overlap + replace icon with favicon cat

--- app/src/main/java/com/mangaxiaocat/reader/MainActivity.java
+++ app/src/main/java/com/mangaxiaocat/reader/MainActivity.java
@@ -56,12 +56,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Hide status bar, go edge-to-edge
-        getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        );
-
         setContentView(R.layout.activity_main);
 
         progressBar  = findViewById(R.id.progressBar);
